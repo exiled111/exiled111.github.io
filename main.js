afd76453a96c7b890b72d561a507b95d0c8c4300
exiled111 <alexander.reeves22@yahoo.de>
2022-07-14 12:01:25 +0100

    V 1.0

---
+++ main.js
@@ -0,0 +1,28 @@
+function generator() {
+    while (w==x || w==y || w==z || x==y || x==z || y==z) {
+        var w= Math.floor((Math.random()*35)+1);
+        var x= Math.floor((Math.random()*35)+1);
+        var y= Math.floor((Math.random()*35)+1);
+        var z= Math.floor((Math.random()*35)+1);
+    }
+    console.log(w,x,y,z);
+    document.getElementById('divImage').innerHTML=` 
+    <div class="row">
+        <div class="column">
+            <img src="img/${w}.jpg" alt="Snow" style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/${x}.jpg" alt="Forest" style="width:100%">
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <img src="img/${y}.jpg" alt="Snow" style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/${z}.jpg" alt="Forest" style="width:100%">
+        </div>
+    </div>
+    
+    `;
+}
